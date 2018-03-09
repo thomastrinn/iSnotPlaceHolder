2fcdc0b8607caf5139c48a9ecebde85a42cd89b6
Herold Benedek <herold.benedek@webstar.hu>
2018-03-09 20:28:36 +0100

    Alap kibővítése (Valence, Engagement)

--- Assets/Scripts/PlayerEmotions.cs
+++ Assets/Scripts/PlayerEmotions.cs
@@ -13,6 +13,8 @@ public class PlayerEmotions : ImageResultsListener
     public Text AngerValue;
     public Text SurpriseValue;
     public Text ContemptValue;
+    public Text ValenceValue;
+    public Text EngagementValue;
 
     private float currentSurprise;
     private float currentContempt;
@@ -21,18 +23,14 @@ public class PlayerEmotions : ImageResultsListener
     private float currentJoy;
     private float currentDisgust;
     private float currentSadness;
+    private float currentValence;
+    private float currentEngagement;
     
     public FeaturePoint[] featurePointsList;
 
     private void Start()
     {
-        JoyValue.text = "Joy: " + (int) currentJoy + "/100";
-        FearValue.text = "Fear: " + (int) currentFear + "/100";;
-        ContemptValue.text = "Contempt: " + (int) currentContempt + "/100";
-        AngerValue.text = "Anger: " + (int) currentAnger + "/100";
-        SurpriseValue.text = "Surprise: " + (int) currentSurprise + "/100";
-        DisgustValue.text = "Disgust: " + (int) currentDisgust + "/100";
-        SadnessValue.text = "Sadness: " + (int) currentSadness + "/100";
+        updateValues();
     }
 
     public override void onFaceFound(float timestamp, int faceId)
@@ -47,13 +45,22 @@ public class PlayerEmotions : ImageResultsListener
 
     public override void onImageResults(Dictionary<int, Face> faces)
     {
-
         foreach (KeyValuePair<int, Face> pair in faces)
         {
-            Debug.Log("Got face results");
-            
             int FaceId = pair.Key;  // The Face Unique Id.
             Face face = pair.Value;    // Instance of the face class containing emotions, and facial expression values.
+
+            Debug.Log("Got face results: " + FaceId);
+
+            currentSurprise = 0;
+            currentContempt = 0;
+            currentAnger = 0;
+            currentFear = 0;
+            currentJoy = 0;
+            currentDisgust = 0;
+            currentSadness = 0;
+            currentValence = 0;
+            currentEngagement = 0;
 
             face.Emotions.TryGetValue(Emotions.Joy, out currentJoy);
             face.Emotions.TryGetValue(Emotions.Fear, out currentFear);
@@ -62,16 +69,23 @@ public class PlayerEmotions : ImageResultsListener
             face.Emotions.TryGetValue(Emotions.Surprise, out currentSurprise);
             face.Emotions.TryGetValue(Emotions.Disgust, out currentDisgust);
             face.Emotions.TryGetValue(Emotions.Sadness, out currentSadness);
+            face.Emotions.TryGetValue(Emotions.Valence, out currentValence);
+            face.Emotions.TryGetValue(Emotions.Engagement, out currentEngagement);
             
-
-            JoyValue.text = "Joy: " + (int) currentJoy + "/100";
-            FearValue.text = "Fear: " + (int) currentFear + "/100";;
-            ContemptValue.text = "Contempt: " + (int) currentContempt + "/100";
-            AngerValue.text = "Anger: " + (int) currentAnger + "/100";
-            SurpriseValue.text = "Surprise: " + (int) currentSurprise + "/100";
-            DisgustValue.text = "Disgust: " + (int) currentDisgust + "/100";
-            SadnessValue.text = "Sadness: " + (int) currentSadness + "/100";
+            updateValues();
         }
+    }
+
+    private void updateValues() {
+        JoyValue.text = "Joy: " + (int) currentJoy + "/100";
+        FearValue.text = "Fear: " + (int) currentFear + "/100";;
+        ContemptValue.text = "Contempt: " + (int) currentContempt + "/100";
+        AngerValue.text = "Anger: " + (int) currentAnger + "/100";
+        SurpriseValue.text = "Surprise: " + (int) currentSurprise + "/100";
+        DisgustValue.text = "Disgust: " + (int) currentDisgust + "/100";
+        SadnessValue.text = "Sadness: " + (int) currentSadness + "/100";
+        ValenceValue.text = "Valence: " + (int) currentValence + "/100";
+        EngagementValue.text = "Engagement: " + (int) currentEngagement + "/100";
     }
 
 }
