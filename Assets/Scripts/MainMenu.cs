787a1c86a476f77b2a5fb2cbf37c030b5f8676b1
Herold Benedek <herold.benedek@webstar.hu>
2018-03-09 11:04:23 +0100

    init project

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenAlapScene() {
+		SceneManager.LoadScene("Alap", LoadSceneMode.single);
+	}
+}
